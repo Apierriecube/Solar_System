9f574126d19d659ec0230692c150fc80717eeed8
Apierriecube <apierriecube.666@gmail.com>
2025-03-04 10:16:27 +0100

    failed rotation, bug fixe and comment

--- src/main/java/fr/univtln/pierre/projet/App.java
+++ src/main/java/fr/univtln/pierre/projet/App.java
@@ -11,7 +11,6 @@ import com.jme3.math.FastMath;
 import com.jme3.math.Vector3f;
 import com.jme3.renderer.queue.RenderQueue;
 import com.jme3.scene.Geometry;
-import com.jme3.scene.Node;
 import com.jme3.scene.shape.Sphere;
 import com.jme3.scene.shape.Torus;
 import com.jme3.system.AppSettings;
@@ -19,6 +18,7 @@ import com.jme3.util.SkyFactory;
 
 public class App extends SimpleApplication implements ActionListener {
 
+  //Initialization of the variables
   private Planet terre;
   private Planet lune;
   private Planet mercure;
@@ -35,15 +35,10 @@ public class App extends SimpleApplication implements ActionListener {
   private Geometry soleilGeom;
   private Geometry ringGeom;
 
-  private Node pivotT;
-
-  private float angle = 0;
-
-
   private boolean lowerCam = false;
   private boolean upperCam = false;
 
-  private int speed = 1;
+  private int vr = 1;
   private boolean pause = false;
 
   /**
@@ -70,19 +65,11 @@ public class App extends SimpleApplication implements ActionListener {
   @Override
   public void simpleInitApp(){
 
-    flyCam.setMoveSpeed(100);
+    //cam speed and render distance
+    flyCam.setMoveSpeed(500);
     cam.setFrustumFar(10000);
 
-    rootNode.attachChild(SkyFactory.createSky(
-      assetManager,
-      assetManager.loadTexture("Textures/Sky/right.png"),
-      assetManager.loadTexture("Textures/Sky/left.png"),
-      assetManager.loadTexture("Textures/Sky/top.png"),
-      assetManager.loadTexture("Textures/Sky/bottom.png"),
-      assetManager.loadTexture("Textures/Sky/front.png"),
-      assetManager.loadTexture("Textures/Sky/back.png")
-    ));
-
+    //input initialization
     inputManager.addMapping("UpperCam", new KeyTrigger(KeyInput.KEY_SPACE));
     inputManager.addListener(this, "UpperCam");
     inputManager.addMapping("LowerCam", new KeyTrigger(KeyInput.KEY_LSHIFT));
@@ -96,18 +83,41 @@ public class App extends SimpleApplication implements ActionListener {
     inputManager.addMapping("Go", new KeyTrigger(KeyInput.KEY_NUMPAD5));
     inputManager.addListener(this, "Go");
 
-    terre = Planet.newInstance(32,32,4f,assetManager, "Earth", 	6371, 5.972168e24);
-    lune = Planet.newInstance(16,16,1f,assetManager, "Moon", 1737.4, 7.346e22);
+    //sky creation
+    rootNode.attachChild(SkyFactory.createSky(
+      assetManager,
+      assetManager.loadTexture("Textures/Sky/right.png"),
+      assetManager.loadTexture("Textures/Sky/left.png"),
+      assetManager.loadTexture("Textures/Sky/top.png"),
+      assetManager.loadTexture("Textures/Sky/bottom.png"),
+      assetManager.loadTexture("Textures/Sky/front.png"),
+      assetManager.loadTexture("Textures/Sky/back.png")
+    ));
 
-    mercure = Planet.newInstance(32,32,1.5f,assetManager, "Mercury", 2439.7, 3.3011e23);
-    venus = Planet.newInstance(32,32,4f,assetManager, "Venus", 6051.8, 4.8675e24);
-    mars = Planet.newInstance(32,32,2f,assetManager, "Mars", 3389.5, 6.4171e23);
-    phobos = Planet.newInstance(32,32,0.1f,assetManager, "Phobos", 11.08, 1.0659e16);
-    deimos = Planet.newInstance(32,32,0.1f,assetManager, "Deimos", 6.27, 1.4762e15);
-    jupiter = Planet.newInstance(32,32,40f,assetManager, "Jupiter", 69911, 1.8982e27);
-    io = Planet.newInstance(32,32,1f,assetManager, "Io", 1821.6, 8.931938e22);
-    europe = Planet.newInstance(32,32,1f,assetManager, "Europe", 1560.8, 4.799844e22);
-    saturn = Planet.newInstance(32,32,38f,assetManager, "Saturn", 58232, 5.6824e26);
+    //planet creation (zsample, radialsample, radius, assetmanager, name, size, weight, rootNode, position) with sun and saturn's ring initialized separately
+    Sphere soleilSphere = new Sphere(128,128,100f); 
+    soleilGeom = new Geometry("Sphere", soleilSphere);  
+    Material soleilMat = new Material(assetManager,"Common/MatDefs/Misc/Unshaded.j3md");
+    soleilMat.setTexture("ColorMap", assetManager.loadTexture("Textures/ColoredTex/Sun.jpg"));
+    soleilGeom.setMaterial(soleilMat);
+    soleilGeom.setLocalTranslation(new Vector3f(0,0,-300));
+    double soleilSize = 696340;
+    double soleilWeight = 1.9885e30;
+    soleilGeom.rotate(-FastMath.PI/2, 0, 0);
+    rootNode.attachChild(soleilGeom);
+
+    mercure = Planet.newInstance(32,32,1.5f,assetManager, "Mercury", 2439.7, 3.3011e23, rootNode, new Vector3f(139,0,-300));
+    venus = Planet.newInstance(32,32,4f,assetManager, "Venus", 6051.8, 4.8675e24, rootNode, new Vector3f(175,0,-300));
+    terre = Planet.newInstance(32,32,4f,assetManager, "Earth", 	6371, 5.972168e24, rootNode, new Vector3f(207,0,-300));
+    lune = Planet.newInstance(16,16,1f,assetManager, "Moon", 1737.4, 7.346e22, rootNode, new Vector3f(207,0,-300));
+    mars = Planet.newInstance(32,32,2f,assetManager, "Mars", 3389.5, 6.4171e23, rootNode, new Vector3f(261,0,-300));
+    phobos = Planet.newInstance(32,32,0.1f,assetManager, "Phobos", 11.08, 1.0659e16, rootNode, new Vector3f(261,2.22f,-300));
+    deimos = Planet.newInstance(32,32,0.1f,assetManager, "Deimos", 6.27, 1.4762e15, rootNode, new Vector3f(261,3.67f,-300));
+    jupiter = Planet.newInstance(32,32,40f,assetManager, "Jupiter", 69911, 1.8982e27, rootNode, new Vector3f(447,0,-300));
+    io = Planet.newInstance(32,32,1f,assetManager, "Io", 1821.6, 8.931938e22, rootNode, new Vector3f(447,0,-300));
+    europe = Planet.newInstance(32,32,1f,assetManager, "Europe", 1560.8, 4.799844e22, rootNode, new Vector3f(447,2.22f,-300));
+    saturn = Planet.newInstance(32,32,38f,assetManager, "Saturn", 58232, 5.6824e26, rootNode, new Vector3f(703,0,-300));
+
     Torus ringShape = new Torus(100, 100, 5, 45);
     ringGeom = new Geometry("SaturnRings", ringShape);
     Material ringMat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
@@ -116,99 +126,35 @@ public class App extends SimpleApplication implements ActionListener {
     ringGeom.setQueueBucket(RenderQueue.Bucket.Transparent);
     ringGeom.setMaterial(ringMat);
     ringGeom.setLocalScale(1, 1, 0.01f);
-    uranus = Planet.newInstance(32,32,16f,assetManager, "Uranus", 25362, 8.681e25);
-    neptune = Planet.newInstance(32,32,15f,assetManager, "Neptune", 24622, 1.02409e26);
-
-    mercure.getGeom().rotate(-FastMath.PI/2, 0, 0);
-    mercure.getGeom().setLocalTranslation(new Vector3f(103,0,0));
-    rootNode.attachChild(mercure.getGeom());
-
-    venus.getGeom().rotate(-FastMath.PI/2, 0, 0);
-    venus.getGeom().setLocalTranslation(new Vector3f(139,0,0));
-    rootNode.attachChild(venus.getGeom());
-
-    lune.getGeom().rotate(-FastMath.PI/2, 0, 0);
-    lune.getGeom().setLocalTranslation(new Vector3f(0,6,0));
-    rootNode.attachChild(lune.getGeom());
-
-    terre.getGeom().rotate(-FastMath.PI/2, 0, 0);
-    rootNode.attachChild(terre.getGeom());
-    pivotT = new Node("pivotT");
-    pivotT.attachChild(lune.getGeom());
-    pivotT.attachChild(terre.getGeom());
-    pivotT.setLocalTranslation(new Vector3f(171,0,0));
-    rootNode.attachChild(pivotT);
-
-    mars.getGeom().rotate(-FastMath.PI/2, 0, 0);
-    mars.getGeom().setLocalTranslation(new Vector3f(225,0,0));
-    rootNode.attachChild(mars.getGeom());
-
-    phobos.getGeom().rotate(-FastMath.PI/2, 0, 0);
-    phobos.getGeom().setLocalTranslation(new Vector3f(225,2.22f,0));
-    rootNode.attachChild(phobos.getGeom());
-
-    deimos.getGeom().rotate(-FastMath.PI/2, 0, 0);
-    deimos.getGeom().setLocalTranslation(new Vector3f(225,2.88f,0));
-    rootNode.attachChild(deimos.getGeom());
-
-    jupiter.getGeom().rotate(-FastMath.PI/2, 0, 0);
-    jupiter.getGeom().setLocalTranslation(new Vector3f(411,0,0));
-    rootNode.attachChild(jupiter.getGeom());
-    
-    io.getGeom().rotate(-FastMath.PI/2, 0, 0);
-    io.getGeom().setLocalTranslation(new Vector3f(411,42.2f,0));
-    rootNode.attachChild(io.getGeom());
-
-    europe.getGeom().rotate(-FastMath.PI/2, 0, 0);
-    europe.getGeom().setLocalTranslation(new Vector3f(411,45.2f,0));
-    rootNode.attachChild(europe.getGeom());
-
-    saturn.getGeom().rotate(-FastMath.PI/2, 0, 0);
-    saturn.getGeom().setLocalTranslation(new Vector3f(667,0,0));
-    rootNode.attachChild(saturn.getGeom());
-
     ringGeom.rotate(-FastMath.PI/2, 0, 0);
-    ringGeom.setLocalTranslation(new Vector3f(667,0,0));
+    ringGeom.setLocalTranslation(new Vector3f(703,0,-300));
     rootNode.attachChild(ringGeom);
 
-    uranus.getGeom().rotate(-FastMath.PI/2, 0, 0);
-    uranus.getGeom().setLocalTranslation(new Vector3f(1189,0,0));
-    rootNode.attachChild(uranus.getGeom());
+    uranus = Planet.newInstance(32,32,16f,assetManager, "Uranus", 25362, 8.681e25, rootNode, new Vector3f(1225,0,-300));
+    neptune = Planet.newInstance(32,32,15f,assetManager, "Neptune", 24622, 1.02409e26, rootNode, new Vector3f(1785,0,-300));
 
-    neptune.getGeom().rotate(-FastMath.PI/2, 0, 0);
-    neptune.getGeom().setLocalTranslation(new Vector3f(1749,0,0));
-    rootNode.attachChild(neptune.getGeom());
+    //velocity initialization
+    //terre.setVelocity(new Vector3f(-2,0,-6.95f));
 
-    Sphere soleilSphere = new Sphere(128,128,64f); 
-    soleilGeom = new Geometry("Sphere", soleilSphere);  
-    Material soleilMat = new Material(assetManager,"Common/MatDefs/Misc/Unshaded.j3md");
-    soleilMat.setTexture("ColorMap", assetManager.loadTexture("Textures/ColoredTex/Sun.jpg"));
-    soleilGeom.setMaterial(soleilMat);
-    double soleilSize = 696340;
-    double soleilWeight = 1.9885e30;
-
-    soleilGeom.rotate(-FastMath.PI/2, 0, 0);
-    rootNode.attachChild(soleilGeom);
-
+    //light initialization
     PointLight lamp_light = new PointLight();
     lamp_light.setColor(ColorRGBA.White);
-    lamp_light.setPosition(new Vector3f(0,0,-75));
+    lamp_light.setPosition(new Vector3f(0,0,-300));
     rootNode.addLight(lamp_light);
 
-    rootNode.setLocalTranslation(0, 0, -75);
   }
 
   @Override
   public void simpleUpdate(float tpf) {
-
+    //set button reactions, rotation and orbit of objects
     if (lowerCam){
-      cam.setLocation(cam.getLocation().add(0, -tpf * 10, 0));
+      cam.setLocation(cam.getLocation().add(0, -tpf * 500, 0));
     }
     if (upperCam){
-      cam.setLocation(cam.getLocation().add(0, tpf * 10, 0));
+      cam.setLocation(cam.getLocation().add(0, tpf * 500, 0));
     }
 
-    tpf *= speed;
+    tpf *= vr;
     if (pause){
       tpf = 0;
     }
@@ -228,16 +174,17 @@ public class App extends SimpleApplication implements ActionListener {
     ringGeom.rotate(0, 0, (float) (0.002*tpf));
     uranus.getGeom().rotate(0, 0, (float) (1.4*tpf));
     neptune.getGeom().rotate(0, 0, (float) (1.5*tpf));
-    
-    /*angle += tpf; 
-    float xterre = FastMath.cos(angle) * 30;
-    float zterre = FastMath.sin(angle) * 30;
-    pivotT.setLocalTranslation(xterre, 0, -75 + zterre);*/
 
+    
+
+    
+    
+    //terre.getGeom().setLocalTranslation(getNextPosition(terre,tpf));
   }
 
   @Override
   public void onAction(String name, boolean isPressed, float tpf){
+    //set button reactions
     if (name.equals("LowerCam")) {
       lowerCam = isPressed;
           
@@ -247,11 +194,11 @@ public class App extends SimpleApplication implements ActionListener {
     }
 
     if (name.equals("Speed")) {
-      speed *= 2;
+      vr *= 2;
     }
     if (name.equals("Slow")) {
-      if (speed > 1){
-        speed /= 2;
+      if (vr > 1){
+        vr /= 2;
       }
     }
     if (name.equals("Stop")) {
@@ -262,5 +209,32 @@ public class App extends SimpleApplication implements ActionListener {
     }
     
   }
+
+  /*public static Vector3f computeAcceleration(Vector3f r) {
+    double rMag = Math.sqrt(r.x * r.x + r.y * r.y + r.z * r.z);
+    double factor = -25000 / (rMag * rMag * rMag);
+
+    return new Vector3f((float) (factor * r.x), (float) (factor * r.y), (float) (factor * r.z));
+  }
+
+public static Vector3f getNextPosition(Planet planet,float tpf) {
+
+    Vector3f r = planet.getGeom().getLocalTranslation();
+    Vector3f a = computeAcceleration(r);
+
+    Vector3f v = planet.getVelocity();
+    
+    v.x += a.x * tpf;
+    v.y += a.y * tpf;
+    v.z += a.z * tpf;
+    
+    r.x += planet.getVelocity().x * tpf;
+    r.y += planet.getVelocity().y * tpf;
+    r.z += planet.getVelocity().z * tpf;
+
+
+    planet.setVelocity(v);
+    return r;
+}*/
   
 }
--- src/main/java/fr/univtln/pierre/projet/Planet.java
+++ src/main/java/fr/univtln/pierre/projet/Planet.java
@@ -3,7 +3,10 @@ package fr.univtln.pierre.projet;
 import com.jme3.asset.AssetManager;
 import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
+import com.jme3.math.FastMath;
+import com.jme3.math.Vector3f;
 import com.jme3.scene.Geometry;
+import com.jme3.scene.Node;
 import com.jme3.scene.shape.Sphere;
 
 import lombok.Getter;
@@ -19,13 +22,16 @@ public class Planet{
     private final double size;
     private final double weight;
 
+    /*@Setter
+    private Vector3f velocity;*/
+
     //Factory
-    public static Planet newInstance(int zsample, int radialsample, float radius, AssetManager assetManager, String name, double size, double weight){
-        return new Planet(zsample, radialsample, radius, assetManager,name, size, weight);
+    public static Planet newInstance(int zsample, int radialsample, float radius, AssetManager assetManager, String name, double size, double weight,Node rootNode, Vector3f pos){
+        return new Planet(zsample, radialsample, radius, assetManager,name, size, weight, rootNode, pos);
     }
 
     //Constructeur sans paramètres
-    private Planet(int zsample,int radialsample,float radius,AssetManager assetManager,String name,double size,double weight){
+    private Planet(int zsample,int radialsample,float radius,AssetManager assetManager,String name,double size,double weight, Node rootNode, Vector3f pos){
         this.sphere = new Sphere(zsample, radialsample, radius);
         this.geom = new Geometry("Sphere", this.sphere);  
         this.mat = new Material(assetManager,"Common/MatDefs/Light/Lighting.j3md");
@@ -34,6 +40,9 @@ public class Planet{
         this.mat.setColor("Ambient", ColorRGBA.White);  
         this.mat.setColor("Diffuse", ColorRGBA.White);
         this.geom.setMaterial(this.mat);
+        this.geom.rotate(-FastMath.PI/2, 0, 0);
+        this.geom.setLocalTranslation(pos);
+        rootNode.attachChild(this.geom);
 
         this.name = name;
         this.size = size;
